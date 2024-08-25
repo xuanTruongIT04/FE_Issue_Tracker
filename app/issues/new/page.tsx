1a845d9331f6a6d3dbff805b7b1a4d0e302a2a5f
Nguyễn Xuân Trường <xuantruong.nguyen@secomus.com>
2024-08-25 14:48:52 +0700

    Implement the client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,28 @@
 'use client';
 
-import { Button, Callout, TextField } from '@radix-ui/themes';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
 import React, { useState } from 'react';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { validationSchema } from '@/app/validationSchema';
+import { z}  from 'zod';
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof validationSchema>
+
+// interface IssueForm {
+//     title: string;
+//     description: string;
+// }
 
 const NewIssuePage = () => {
     const router = useRouter();
-    const { register, control, handleSubmit } = useForm<IssueForm>();
+    const { register, control, handleSubmit, formState: { errors, isDirty, isValid } } = useForm<IssueForm>({
+        resolver: zodResolver(validationSchema)
+    });
     const [error, setError] = useState('');
 
     return (
@@ -37,6 +44,7 @@ const NewIssuePage = () => {
                 })}
             >
                 <TextField.Root placeholder="Title" {...register('title')} />
+                {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
                 <Controller
                     name="description"
                     control={control}
@@ -44,6 +52,7 @@ const NewIssuePage = () => {
                         <SimpleMDE placeholder="Description" {...field} />
                     )}
                 />
+                {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
 
                 <Button>Submit New Issue</Button>
             </form>
